2e13a1d2d3ef4388928952b6fea727caa2ea2811
guolisen <senecho@163.com>
2024-07-07 16:53:22 +0800

    修改crates插件

--- lua/guolisen/plugins/crates.lua
+++ lua/guolisen/plugins/crates.lua
@@ -2,6 +2,22 @@ return {
   'saecki/crates.nvim',
   event = { "BufRead Cargo.toml" },
   config = function()
-    require('crates').setup()
+    require('crates').setup({
+      completion = {
+        cmp = {
+          enabled = true,
+          use_custom_kind = true,
+          kind_text = {
+            version = "Version",
+            feature = "Feature",
+          }
+        },
+        crates = {
+          enabled = true,  -- disabled by default
+          max_results = 8, -- The maximum number of search results to display
+          min_chars = 3,   -- The minimum number of charaters to type before completions begin appearing
+        },
+      }
+    })
   end,
 }
